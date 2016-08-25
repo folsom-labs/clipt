0c91a223c8fb1730295b59faf1744947c8740e36
Paul Gibbs <paul.gibbs@folsomlabs.com>
2016-08-24 23:44:42 -0700

    override default clipper error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-
+import Logger from 'js-logger';
 import ClipperLib from './clipperlib/clipper';
+
+Logger.useDefaults({
+    defaultLevel:  Logger.WARN,
+});
+
+const logger = Logger.get('clipper');
+
+ClipperLib.Error = (message) => logger.warn(message);
 
 const clipt = {
     ClipperLib,
